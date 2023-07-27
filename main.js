a0d51a07332ca0d2fd25fbeff9cd3e2df04a4852
NowWeAre-Free <kontodarm@wp.pl>
2023-07-27 21:56:04 +0200

    test closuers

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+const { noConflict, negate } = require("cypress/types/lodash");
+
 let number = 0;
 
 document.addEventListener("click", () => {
   number++;
   document.body.textContent = `Aktualny stan licznika to ${number}`;
 });
+
+function x() {
+  let n = 0;
+  function y() {
+    console.log(n);
+  }
+  return y;
+}
+
+const example = x();
