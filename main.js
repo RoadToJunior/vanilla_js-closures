8b850f2d2f77231a2b97a3393d2ef366c06c02a0
NowWeAre-Free <kontodarm@wp.pl>
2023-07-30 21:01:13 +0200

    timer

--- main.js
+++ main.js
@@ -15,34 +15,49 @@
 
 // const example = x();
 
-let number = 0;
+// let number = 0;
 
-const add = (start = 0) => {
-  let number = start;
+// const add = (start = 0) => {
+//   let number = start;
+//   return () => {
+//     number++;
+//     document.body.textContent = `Stan licznika to ${number}`;
+//   };
+// };
+
+// const counter = add();
+// const counterFrom5 = add(5);
+
+// document.addEventListener("click", counter);
+
+// const user = (user = "", age) => {
+//   const userName = user;
+//   const userAge = age;
+
+//   function showName() {
+//     console.log(
+//       `Użytkownik ${userName} ${
+//         userAge >= 18
+//           ? "ma więcej niż 18 lat i może kupić piwo"
+//           : "nie może kupić piwa"
+//       } `
+//     );
+//     return showName;
+//   }
+// };
+// const marcin = user("Marcin", 26);
+// marcin();
+
+//zadanie TIMER
+
+const timer = () => {
+  let number = 0;
+  document.body.textContent = `${number} sekund`;
   return () => {
     number++;
-    document.body.textContent = `Stan licznika to ${number}`;
+    document.body.textContent = `${number} sekund`;
   };
 };
+const clock = timer();
 
-const counter = add();
-const counterFrom5 = add(5);
-
-document.addEventListener("click", counter);
-
-const user = () => {
-  const name = "Mieszko";
-  const age = 16;
-
-  function showName() {
-    console.log(
-      `Użytkownik ${name} ma ${
-        age > 18 ? "więcej niż 18 lat i może kupić piwo" : "nie może kupić piwa"
-      } `
-    );
-  }
-  showName();
-};
-
-const person = user();
-person;
+setInterval(clock, 1000);
